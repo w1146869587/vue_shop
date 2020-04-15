927556995ef4f5ef631d43b3f6ca8af522200bdc
w1146869587 <1146869587@qq.com>
2020-04-15 17:36:36 +0800

    '用户管理'

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,23 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/users.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/Users', component: Users }
+    ]
+  }
 
 ]
 
@@ -18,10 +28,10 @@ const router = new VueRouter({
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-// to 将要访问的路径
-// from 代表从哪个路径跳转而来
-// next一个函数，表示放行
-// next()放行，next('/login') 强制跳转
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next一个函数，表示放行
+  // next()放行，next('/login') 强制跳转
   if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
