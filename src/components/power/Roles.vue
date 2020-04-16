aa3dde692f6777be8d6977c202e021093e55d58c
w1146869587 <wah1146869587@126.com>
2020-04-16 22:21:30 +0800

    mm

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,15 +1,95 @@
 <template>
-    <div>
-        <h1>角色列表组件</h1>
-    </div>
+  <div>
+    <!-----面包屑导航区域-------->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!----卡片视图区域------------------>
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+
+      <!----角色列表----->
+      <el-table :data="rolelist" border stripe>
+        <!-----展开列------>
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row
+              :class="['bdbottom', i1 === 0 ? 'bdtop' : '']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+            >
+              <!---渲染一级权限--->
+              <el-col :span="5">
+                <el-tag>{{ item1.authName }}</el-tag>
+                 <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!---渲染二级和三级权限--->
+              <el-col :span="19"></el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <!--索引列--->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作">
+          <template width="300px">
+            <el-button size="mini" type="primary" icon="el-icon-edit"
+              >编辑</el-button
+            >
+            <el-button size="mini" type="danger" icon="el-icon-delete"
+              >删除</el-button
+            >
+            <el-button size="mini" type="warning" icon="el-icon-setting"
+              >分配权限</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      rolelist: []
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList() {
+      const { data: res } = await this.$http.get('roles')
+      console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.rolelist = res.data
+      console.log(this.rolelist)
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
 
+.bdtop {
+  border-top: 1px solid #eee;
+}
+
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://192.168.11.129:8888/api/private/v1/'
 // 拦截器
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
